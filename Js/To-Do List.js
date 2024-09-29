862ebf13ceb1fbd4641c793ffe5bb390f9f84c2b
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:15:44 +0430

    My Initial Commit

---
+++ Js/To-Do List.js
@@ -0,0 +1,9 @@
+const todos = ['Learn JavaScript', 'Build a project', 'Celebrate success'];
+todos.forEach((todo, index) => {
+    console.log(`${index + 1}: ${todo}`);
+});
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
