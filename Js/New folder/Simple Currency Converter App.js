c456add7a0cf8d2c8716973bacbdd4027b73bdad
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:27:52 +0430

    My Initial Commit

---
+++ Js/New folder/Simple Currency Converter App.js
@@ -0,0 +1,13 @@
+function convertCurrency(amount, exchangeRate) {
+    return amount * exchangeRate;
+}
+
+const amountInUSD = 100;
+const exchangeRate = 0.85; // EUR to USD
+console.log(`Equivalent amount in EUR: ${convertCurrency(amountInUSD, exchangeRate)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
