013fa8d89f250d8288066b2a6b7a879713082a99
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:15:24 +0430

    My Initial Commit

---
+++ Js/Random Number Generator.js
@@ -0,0 +1,7 @@
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log('Random Number:', randomNumber);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
