3036593727ed54c82ea31f8ab4f4b7c3fa9458b3
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:27:33 +0430

    My Initial Commit

---
+++ Js/New folder/Simple Calculator GUI.js
@@ -0,0 +1,11 @@
+function calculate() {
+    const num1 = parseFloat(document.getElementById("num1").value);
+    const num2 = parseFloat(document.getElementById("num2").value);
+    const result = num1 + num2;
+    document.getElementById("result").innerText = "Result: " + result;
+}
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
