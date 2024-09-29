fe10431b6efb283f510f9e98c487d579d2a945c8
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:28:54 +0430

    My Initial Commit

---
+++ Js/New folder/Scissors Game.js
@@ -0,0 +1,11 @@
+const choices = ["rock", "paper", "scissors"];
+
+function playGame(userChoice) {
+    const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+    // Determine the winner and display the result on the GUI
+}
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
