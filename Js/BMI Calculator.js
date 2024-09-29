a10d7efaaf68737f31d5a2da7c7f5ed10f77ba47
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:14:59 +0430

    My Initial Commit

---
+++ Js/BMI Calculator.js
@@ -0,0 +1,10 @@
+const weight = 70; // in kg
+const height = 1.75; // in meters
+const bmi = weight / (height * height);
+console.log(`BMI is: ${bmi.toFixed(2)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
