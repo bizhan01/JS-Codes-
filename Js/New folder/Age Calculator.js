f745088a220eec33dbebaf5f2bb43205dd5bb515
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:24:46 +0430

    My Initial Commit

---
+++ Js/New folder/Age Calculator.js
@@ -0,0 +1,7 @@
+function startTest() {
+    // Start the typing test and calculate the typing speed
+}
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
