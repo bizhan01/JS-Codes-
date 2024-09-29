b0e8e743fa6ef704e95b34e968a5bfc8c2d6e56c
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:22:17 +0430

    My Initial Commit

---
+++ Js/New folder/Random Dice Roll App.js
@@ -0,0 +1,11 @@
+function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+console.log(`Dice 1: ${rollDice()}`);
+console.log(`Dice 2: ${rollDice()}`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
