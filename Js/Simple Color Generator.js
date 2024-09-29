f9874bc2f893957bd607bd28d7ef84619f762638
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:15:34 +0430

    My Initial Commit

---
+++ Js/Simple Color Generator.js
@@ -0,0 +1,11 @@
+function generateRandomColor() {
+    const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    return randomColor;
+}
+
+console.log('Random Color:', generateRandomColor());
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
