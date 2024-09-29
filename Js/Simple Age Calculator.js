67c7ea904bf0d6d07850f58357d92f3db74e7461
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:17:51 +0430

    My Initial Commit

---
+++ Js/Simple Age Calculator.js
@@ -0,0 +1,9 @@
+const birthYear = 1990;
+const currentYear = new Date().getFullYear();
+const age = currentYear - birthYear;
+console.log(`You are ${age} years old.`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
