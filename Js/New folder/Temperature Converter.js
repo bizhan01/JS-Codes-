bff29b4ff5633b06dac6ba280762265a7af71b1f
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:23:45 +0430

    My Initial Commit

---
+++ Js/New folder/Temperature Converter.js
@@ -0,0 +1,11 @@
+function convertTemperature() {
+    const celsius = parseFloat(document.getElementById("celsius").value);
+    const fahrenheit = (celsius * 9/5) + 32;
+    document.getElementById("fahrenheit").innerText = "Fahrenheit: " + fahrenheit;
+}
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
