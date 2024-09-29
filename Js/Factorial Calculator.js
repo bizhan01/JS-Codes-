83dba06f07cb36a773ea9dfad90c78247afa8fbc
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:15:12 +0430

    My Initial Commit

---
+++ Js/Factorial Calculator.js
@@ -0,0 +1,13 @@
+function factorial(n) {
+    if (n === 0 || n === 1) return 1;
+    return n * factorial(n - 1);
+}
+
+const num = 5;
+console.log(`Factorial of ${num} is: ${factorial(num)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
