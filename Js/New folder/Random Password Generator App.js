c952a4b762b11442789e23da464d5390c96400b7
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:25:46 +0430

    My Initial Commit

---
+++ Js/New folder/Random Password Generator App.js
@@ -0,0 +1,15 @@
+function generateRandomPassword(length) {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let password = "";
+    for (let i = 0; i < length; i++) {
+        password += charset.charAt(Math.floor(Math.random() * charset.length));
+    }
+    return password;
+}
+
+console.log(`Random Password: ${generateRandomPassword(8)}`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
