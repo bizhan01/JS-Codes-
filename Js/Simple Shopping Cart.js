8403b685e9c90b12c8a8ea2e3e116f864d01e409
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:15:59 +0430

    My Initial Commit

---
+++ Js/Simple Shopping Cart.js
@@ -0,0 +1,8 @@
+const cart = [];
+cart.push('Apple', 'Banana', 'Orange');
+console.log('Items in the cart:', cart);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
