903f8d782ec8d8b80eb3ae733b1f968d799e44aa
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:28:15 +0430

    My Initial Commit

---
+++ Js/New folder/Todo List GUI.js
@@ -0,0 +1,10 @@
+document.getElementById('addBtn').addEventListener('click', function() {
+    const task = document.getElementById('taskInput').value;
+    // Add task to the todo list on the GUI
+});
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
