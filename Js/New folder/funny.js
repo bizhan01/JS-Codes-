5056f71bebee41722c19573c5af4f19122a956d2
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:20:28 +0430

    My Initial Commit

---
+++ Js/New folder/funny.js
@@ -0,0 +1,26 @@
+setTimeout(() => {
+    alert("Hey there! Don't touch me there!");
+}, 30);
+
+
+
+console.log("I'm a console log message. Surprise! 🎉");
+
+
+
+const insults = ['You smell like a wet dog.', 'Your face looks like a pizza.', 'Your coding skills are older than the dinosaurs.'];
+const randomInsult = insults[Math.floor(Math.random() * insults.length)];
+console.log(randomInsult);
+
+
+// while(true) {
+//     console.log("Are we there yet?");
+// }
+
+
+alert([... '🧙‍♂️🐉🏰🔮💰'].reverse().join(''));
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
