015805746845ac7eb4c383dfe65a7c3717dce3b7
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:18:12 +0430

    My Initial Commit

---
+++ Js/Simple Timer.js
@@ -0,0 +1,10 @@
+let seconds = 0;
+setInterval(() => {
+    console.log(seconds);
+    seconds++;
+}, 1000);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
