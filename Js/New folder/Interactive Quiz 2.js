d17c549b3f9bc2e28daac32eab7a7a257b2e2c4e
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:21:32 +0430

    My Initial Commit

---
+++ Js/New folder/Interactive Quiz 2.js
@@ -0,0 +1,19 @@
+const questions = [
+    { question: "What is 2 + 2?", answer: "4" },
+    { question: "Who is the current US President?", answer: "Joe Biden" }
+];
+let currentQuestionIndex = 0;
+
+function displayQuestion() {
+    // Display the next question from the array on the GUI
+}
+
+function checkAnswer() {
+    // Check if the user's answer matches the correct answer
+}
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
