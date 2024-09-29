11c7b8985ec2dfdfe8f0afe4316c48318d867016
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:25:15 +0430

    My Initial Commit

---
+++ Js/New folder/Tip Calculator App.js
@@ -0,0 +1,13 @@
+function calculateTip(amount, tipPercentage) {
+    return amount * (tipPercentage / 100);
+}
+
+const billAmount = 50;
+const tipPercentage = 15;
+console.log(`Tip amount: $${calculateTip(billAmount, tipPercentage)}`);
+
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
