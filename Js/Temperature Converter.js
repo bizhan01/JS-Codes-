407cbc9af3f9ca234b1f62711ad830c0754b6a84
bizhan01 <rahmatullahbizhan@gmail.com>
2024-09-29 13:18:32 +0430

    My Initial Commit

---
+++ Js/Temperature Converter.js
@@ -0,0 +1,8 @@
+const celsius = 30;
+const fahrenheit = (celsius * 9/5) + 32;
+console.log(`${celsius}°C is equal to ${fahrenheit}°F`);
+
+
+// javascript tutorial code
+// Author: Rahmatullah Bizhan
+// Email: rahmatullahbizhan@gmail.com
